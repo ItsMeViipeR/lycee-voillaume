339e7201a313c49b63842fcb03d426a858a2871b
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-11-09 18:37:37 +0100

    feat: :tada: Init Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Accueil - Lycée Voillaume",
+  description: "Site officiel du lycée Voillaume",
+};
+
+export default function Home() {
+  return <></>;
+}
